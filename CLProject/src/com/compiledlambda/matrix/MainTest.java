d747e679274050a8433878082e000af2a13be986
Denys Bez <denysbez.ua@gmail.com>
2015-07-04 22:14:35 +0800

    matrix operations

--- CLProject/src/com/compiledlambda/matrix/MainTest.java
+++ CLProject/src/com/compiledlambda/matrix/MainTest.java
@@ -3,15 +3,27 @@ package com.compiledlambda.matrix;
 public class MainTest {
 
 	public static void main(String[] args) {
-		Matrix matrix = new Matrix(10, 10);
-		System.out.println(matrix.isSquare());
-		for(int row = 0; row < 10; ++row) {
-			for(int column = 0; column < 10; column++) {
-				matrix.set(row, column, row);
+		int n = 2;
+		int m = 2;
+		Matrix matrix = new Matrix(n, m);
+		System.out.println("is square: " + matrix.isSquare());
+		
+		for(int row = 0; row < n; ++row) {
+			for(int column = 0; column < m; column++) {
+				matrix.set(row, column, row + 1 + column);
 			}
 		}
 		
-		System.out.println("is square: " + matrix);
+		System.out.println("det(A): " + matrix.det());
+		
+		System.out.println(matrix);
+		
+		System.out.println(matrix.scalarMultiply(2));
+		
+		System.out.println("det(A): " + matrix.det());
+		
+		System.out.println("inverse:" + matrix.inverse());
+		
 	}
 
 }
--- CLProject/src/com/compiledlambda/matrix/Matrix.java
+++ CLProject/src/com/compiledlambda/matrix/Matrix.java
@@ -19,16 +19,82 @@ public class Matrix {
 		this.matrix = storage;
 	}
 	
-	public void set(int i, int j, double value) {
-		matrix[j][i] = value;
+	public void set(int row, int column, double value) {
+		this.matrix[row][column] = value;
 	}
 	
-	public double ent(int i, int j) {
-		return matrix[j][i];
+	/**
+	 * Element in the row, column position of Matrix
+	 */
+	public double ent(int row, int column) {
+		return this.matrix[row][column];
+	}
+	
+	/**
+	 * Computes determinant
+	 */
+	public double det() {
+		if(!isSquare()) {
+			throw new IllegalArgumentException("determinant can be computed only for a square matrix");
+		}
+		
+		return (ent(0, 0) * ent(1, 1)) - (ent(0, 1) * ent(1, 0));
 	}
 	
 	public boolean isSquare() {
-		return matrix.length == matrix[0].length;
+		return getRowsLength() == getColumntLength();
+	}
+	
+	/**
+	 * Tests if square matrix is not invertible, i.e. singular or degenerate
+	 * 
+	 * TODO: A square matrix is singular if and only if its determinant is 0
+	 */
+	public boolean isSingular() {
+		if(!isSquare()) {
+			return false;
+		}
+		//TODO:
+		return false;
+	}
+
+	public int getColumntLength() {
+		return this.matrix.length;
+	}
+	
+	public int getRowsLength() {
+		return this.matrix[0].length;
+	}
+	
+
+	/**
+	 * Scalar multiplication, changes state of current matrix
+	 */
+	public Matrix scalarMultiply(double scalar) {
+		for(int row = 0; row < getRowsLength(); ++row) {
+			for(int column = 0; column < getColumntLength(); ++column) {
+				set(row, column, scalar * ent(row, column));
+			}
+		}
+		
+		return this;
+	}
+	
+	public Matrix inverse() {
+		double det = det();
+		if(det != 0) {
+			if(getRowsLength() == 2 && getColumntLength() == 2) {
+				final Matrix inverted = new Matrix(getRowsLength(), getColumntLength());
+				inverted.set(0, 0, this.ent(1, 1));
+				inverted.set(0, 1, (-1) * this.ent(0, 1));
+				inverted.set(1, 0, (-1) * this.ent(1, 0));
+				inverted.set(1, 1, this.ent(0, 0));
+				inverted.scalarMultiply(1 / det);
+				return inverted;
+			}			
+		}
+		throw new UnsupportedOperationException("inverse calculation not supported for non-2x2 matrices");
+
 	}
 	
 	
