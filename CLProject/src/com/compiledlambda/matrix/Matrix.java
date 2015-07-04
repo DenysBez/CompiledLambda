ac7308ff5c0cfbc7f92aec886d85d7ea23d02e1a
Denys Bez <denysbez.ua@gmail.com>
2015-07-04 21:03:45 +0800

    matrix update

--- CLProject/src/com/compiledlambda/matrix/Matrix.java
+++ CLProject/src/com/compiledlambda/matrix/Matrix.java
@@ -1,20 +1,30 @@
 package com.compiledlambda.matrix;
-/**
- * 
- */
 
-/**
- * @author Hp
- *
- */
 public class Matrix {
 
-	/**
-	 * @param args
-	 */
+	private final double[][] matrix;
+	
+	public Matrix(int rows, int columns) {
+		if(rows <= 0 || columns <= 0) {
+			throw new IllegalArgumentException("Cannot create matrix with zero rows or columns");
+		}			
+			 
+		double[][] storage = new double[rows][];
+		for(int i = 0; i < storage.length; ++i) {
+			storage[i] = new double[columns];
+		}
+		this.matrix = storage;
+	}
+	
+	
+	public boolean isSquare() {
+		return matrix.length == matrix[0].length;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Matrix matrix = new Matrix(4, 3);
+		System.out.println(matrix.isSquare());
 	}
 
 }
