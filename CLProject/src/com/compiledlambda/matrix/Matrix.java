0b7a0b05b6392132efba77157f3895d1658b4b0c
Denys Bez <denysbez.ua@gmail.com>
2015-07-04 21:01:35 +0800

    Signed-off-by: Denys Bez <denysbez.ua@gmail.com>

---
+++ CLProject/src/com/compiledlambda/matrix/Matrix.java
@@ -0,0 +1,20 @@
+package com.compiledlambda.matrix;
+/**
+ * 
+ */
+
+/**
+ * @author Hp
+ *
+ */
+public class Matrix {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
